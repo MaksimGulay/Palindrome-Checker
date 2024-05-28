e9993bc95321b652c2920f137603cbda5f41d82d
Maksim Gulay <maksimgulaj@gmail.com>
2024-05-28 20:51:16 +0300

    finish task

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
-const input = document.getElementById("text-input");
-const checkBtn = document.getElementById("check-btn");
-const result = document.getElementById("result");
+// document.getElementById("check-btn").addEventListener("click", function () {
+//   const inputText = document.getElementById("text-input").value;
+//   const normalizedText = inputText.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+//   const reversedText = normalizedText.split("").reverse().join("");
+//   const isPalindrome = normalizedText === reversedText;
+//   const result = document.getElementById("result");
+//   if (isPalindrome) {
+//     result.textContent = `"${inputText}" is a palindrome.`;
+//   } else {
+//     result.textContent = `"${inputText}" is not a palindrome.`;
+//   }
+// });
 
-console.log(input);
-function btnClick() {
-  checkBtn.addEventListener("click", () => {
-    console.log(input.value);
-  });
-}
-btnClick();
+const result = document.getElementById("result");
+const button = document.getElementById("check-btn");
+
+button.addEventListener("click", function () {
+  const inputText = document.getElementById("text-input").value;
+  const normalizedText = inputText.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  const reversedText = normalizedText.split("").reverse().join("");
+  const isPalindrome = normalizedText === reversedText;
+  if (isPalindrome) {
+    result.textContent = `"${inputText}" is a palindrome.`;
+  } else {
+    result.textContent = `"${inputText}" is not a palindrome.`;
+  }
+});
